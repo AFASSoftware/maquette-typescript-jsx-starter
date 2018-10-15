ef371b00423310e6cea041568393e86ff83637b0
JC Franco <jfranco@esri.com>
2018-10-15 09:56:35 -0700

    Update maquette-jsx to v2.1.2-0.
    * Set up and fix linting.

--- src/application.tsx
+++ src/application.tsx
@@ -1,21 +1,22 @@
-import {createTextInput} from './components/text-input';
+import { createTextInput } from './components/text-input';
+import { jsx } from 'maquette-jsx';
 
 let name = ''; // Piece of data
 
 let nameInput = createTextInput({
   placeholder: 'What is your name?',
-  getValue: () => name, 
+  getValue: () => name,
   setValue: (value: string) => {
     name = value;
   }
 });
 
-// This function uses the 'jsx' notation 
-// to create the virtual DOM. 
+// This function uses the 'jsx' notation
+// to create the virtual DOM.
 export function render() {
   return <div>
     { nameInput.render() }
-    <p class="output">
+    <p class='output'>
       Hello {name || 'you'}!
     </p>
   </div>;
--- src/components/text-input.tsx
+++ src/components/text-input.tsx
@@ -1,24 +1,26 @@
+// test
+
 import { MaquetteComponent } from 'maquette';
+import { jsx } from 'maquette-jsx';
 
 export interface TextInputConfig {
   placeholder: string;
-  getValue: () => string;
-  setValue: (value: string) => void;
+  getValue(): string;
+  setValue(value: string): void;
 }
 
-export let createTextInput = (config: TextInputConfig) : MaquetteComponent => {
-  
+export let createTextInput = (config: TextInputConfig): MaquetteComponent => {
   function handleInput(evt: Event) {
-    config.setValue((evt.target as HTMLInputElement).value)
+    config.setValue((evt.target as HTMLInputElement).value);
   }
-  
+
   return {
     render: () => {
-      return <input 
-        type="text" 
+      return <input
+        type='text'
         placeholder={config.placeholder}
-        value={config.getValue()} 
+        value={config.getValue()}
         oninput={handleInput} />;
     }
-  }
+  };
 };
