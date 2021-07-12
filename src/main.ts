4b05e6134b0f8dea42f7bf5aee40b9d880b3d8ed
Johan Gorter <johan-gorter@users.noreply.github.com>
2021-07-12 15:23:35 +0200

    Newest depedencies, prettier and eslint

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { createProjector } from 'maquette';
-import { render as renderApplication } from './application';
+import { createProjector } from "maquette";
+
+import { render as renderApplication } from "./application";
 
 createProjector({}).append(document.body, renderApplication);
